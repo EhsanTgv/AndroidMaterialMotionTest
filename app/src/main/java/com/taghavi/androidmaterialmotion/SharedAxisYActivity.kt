a525cbe01cc0def7f3dead90499fae6698b697dd
EhsanTgv <EhsanTgv@gmail.com>
2020-12-09 22:49:24 +0330

    implement SharedAxisYActivity activity & xml design

---
+++ app/src/main/java/com/taghavi/androidmaterialmotion/SharedAxisYActivity.kt
@@ -0,0 +1,12 @@
+package com.taghavi.androidmaterialmotion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SharedAxisYActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shared_axis_y)
+    }
+}
