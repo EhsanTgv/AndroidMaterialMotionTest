4ebf10fd879aaf15d0cb9759d47f381630a3300f
EhsanTgv <EhsanTgv@gmail.com>
2020-12-10 12:11:39 +0330

    Complete PlanetFragment

--- app/src/main/java/com/taghavi/androidmaterialmotion/PlanetFragment.kt
+++ app/src/main/java/com/taghavi/androidmaterialmotion/PlanetFragment.kt
@@ -6,11 +6,55 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
+private const val ARG_PLANET_ID = "planet_id"
+
 class PlanetFragment : Fragment() {
+    private var _binding: PlanetFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    private var planetId: Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            planetId = it.getInt(ARG_PLANET_ID)
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_planet, container, false)
+        _binding = PlanetFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val planet = planets.singleOrNull { it.id == planetId }
+        planet?.let {
+            Glide.with(requireContext())
+                .load(it.mainImage)
+                .into(binding.mainImage)
+
+            binding.planetName.text = planet.name
+            binding.planetDescription.text = planet.description
+        }
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(planetId: Int) =
+            PlanetFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_PLANET_ID, planetId)
+                }
+            }
     }
 }
