5f8e74d663806dc2926f4876930b83ce57995ec0
EhsanTgv <EhsanTgv@gmail.com>
2020-12-10 11:59:03 +0330

    Implement PlanetFragment & xml design

---
+++ app/src/main/java/com/taghavi/androidmaterialmotion/PlanetFragment.kt
@@ -0,0 +1,16 @@
+package com.taghavi.androidmaterialmotion
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class PlanetFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_planet, container, false)
+    }
+}
